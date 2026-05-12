720e5b3f906495ba9c155c7a9b763ce01a92cea3
fahdimas <fahdimas42@gmail.com>
2026-05-12 14:41:50 +0700

    updatee

--- config/database.php
+++ config/database.php
@@ -5,7 +5,7 @@
 
 $serverName = "."; 
 $connectionInfo = array(
-    "Database" => "ApotekHabibah", // Nama database baru yang kita buat tadi
+    "Database" => "db_apotek", // Nama database baru yang kita buat tadi
     "UID" => "", 
     "PWD" => "",
     "CharacterSet" => "UTF-8"
